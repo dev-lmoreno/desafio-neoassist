38410537ef2db8a05e37d467dd182882b8ea8f93
Lucas Moreno <lucasacm007@gmail.com>
2019-01-15 15:00:50 -0200

    Model more information (80%)

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TicketsController extends Controller
+{
+    public function GetTickets(){
+		$tickets = $this->ReadJSON();                                                                                                                      
+		return view('ticket',compact('tickets'));
+	}
+
+	public function ReadJSON(){
+		
+	    $jsonString = file_get_contents(public_path('json/tickets.json'));
+	    $data = json_decode($jsonString, true);
+	    return $data;	
+	}
+
+	public function Priority(){
+		$tickets = $this->ReadJson();
+		$count = [];
+		//SUBJECT
+	    $reclameaqui  = 'reclameAqui';
+		$procon      = 'procon';
+		$reclamacao = 'Reclamação';
+		$resposta = 'RE: ';
+		//SENDER
+		$customer = 'Customer';
+		$expert = 'Expert';
+		foreach($tickets as $key => $ticket){
+			$interactions = $ticket['Interactions'];
+			$datecreate = $ticket['DateCreate'];
+			$dateupdate = $ticket['DateUpdate'];
+			$count[$key] = 0;
+			foreach($interactions as $keyInteractions => $interaction){
+				$subject = $interaction['Subject'];
+				$sender = $interaction['Sender'];
+				if (strpos($sender, $customer) !== false) {
+					$count[$key]+= 20;
+				}
+				if (strpos($sender, $expert) !== false) {
+					$count[$key]-= 10;
+				}
+				if (strpos($subject, $resposta) !== false && strpos($sender, $customer) !== false) {
+					$count[$key]+= 15;
+				}
+				if (strpos($subject, $resposta) !== false && strpos($sender, $expert) !== false) {
+					$count[$key]-= 10;
+				}
+				if (strpos($subject, $reclamacao) !== false && strpos($sender, $customer) !== false){
+					$count[$key]+= 35;
+				}
+				if (strpos($subject, $reclamacao) !== false && strpos($sender, $expert) !== false){
+					$count[$key]-= 25;
+				}
+			}
+
+	        $dateI = date_create($datecreate);
+	        $dateF = date_create($dateupdate);
+	        $diff = date_diff($dateI,$dateF); 
+	        $dateDif = $diff->format("%a");	  
+	       
+	        if ($dateDif >= 30 && strpos($subject, $resposta) !== false) {
+	        	$count[$key]+=20;
+	        }
+	
+	     
+			if($count[$key] >= 35){
+				$ticketPriority = 'Alta';
+				$string = file_get_contents(public_path('json/tickets.json'));
+				$json = json_decode($string, true);
+				$json[$key]["ticketPriority"][] = $ticketPriority;
+				if($json[$key]["ticketPriority"][] = $ticketPriority){
+					echo "já existe essa prioridade";
+				}else{
+					$fp = fopen(public_path('json/tickets.json'), 'w');
+					fwrite($fp, json_encode($json));
+					fclose($fp);					
+				}
+		    } else{
+		        echo "PRIORIDADE NORMAL<br>";
+		        $ticketPriority = 'Normal';
+				$string = file_get_contents(public_path('json/tickets.json'));
+				$json = json_decode($string, true);
+				$json[$key]["ticketPriority"][] = $ticketPriority;
+				if($json[$key]["ticketPriority"][] = $ticketPriority){
+					echo "já existe essa prioridade";
+				}else{
+					$fp = fopen(public_path('json/tickets.json'), 'w');
+					fwrite($fp, json_encode($json));
+					fclose($fp);					
+				}
+		    }
+		}
+	}
+}
---
+++ resources/views/ticket.blade.php
@@ -0,0 +1,122 @@
+<html>
+    <title>TICKET</title>
+    <head>
+        <meta charset="UTF-8">
+
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.min.js"
+          integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+          crossorigin="anonymous"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  
+
+    </head>
+    <body>
+            <h2 class="margin-top-0 text-primary">Tickets Info</h2>
+            <br>
+                <table id="datatable" class="mdl-data-table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Index</th>
+                            <th>TicketID</th>
+                            <th>CategoryID</th>
+                            <th>CustomerID</th>
+                            <th>CustomerName</th>
+                            <th>CustomerEmail</th>
+                            <th>DateCreate</th>
+                            <th>DateUpdate</th>
+                            <th>TicketPriority</th>
+                            <th>More information</th>
+                        </tr>
+                    </thead>
+                    <tbody>            
+                        @foreach ($tickets as $key => $value)
+                            <tr class="tickets" id={{$key}}">
+                                <td>{{$key}}</td>
+                                <td>{{$value["TicketID"]}}</td>
+                                <td>{{$value["CategoryID"]}}</td>
+                                <td>{{$value["CustomerID"]}}</td>
+                                <td>{{$value["CustomerName"]}}</td>
+                                <td>{{$value["CustomerEmail"]}}</td>
+                                <td>{{$value["DateCreate"]}}</td>
+                                <td>{{$value["DateUpdate"]}}</td>  
+                                <td>{{$value["ticketPriority"][0]}}</td>
+                                <td class="text-center"><button onclick="openTicket('{{$key}}')"
+                                 id ="{{$key}}" type="button" class="btn btn-secondary" data-toggle="modal" data-target="#key{{$key}}"><i class="fas fa-plus"></i></button></td> 
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>  
+
+                <div class="modal fade bd-example-modal-lg" id="#key{{$key}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th id="subject">Subject</th>
+                                            <th id="message">Message</th>
+                                            <th id="datecreate">DateCreate</th>
+                                            <th id="sender">Sender</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="modalTable">
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                             
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    $(document).ready( function () {
+    $('#datatable').DataTable( {
+        "info" : false,
+        "search" : true,
+        "paging": true,
+        "order": [ 6, 'asc' ],
+        language: {
+            searchPlaceholder: "Search..."
+        },          
+        columnDefs: [
+            {
+                className: 'mdl-data-table__cell--non-numeric'
+            }
+        ]
+    } );
+    } );   
+
+    function openTicket(id){
+        //alert(id);
+        $.ajax({
+            type: "GET",
+            url: "{{route('readjsonticket')}}",
+            data: {id: id},
+            dataType:'JSON', 
+            success: function(response){
+                response[id].Interactions.map(function(value,index){ 
+                    console.log(value.Message);
+                    $("#modalTable").append(`
+                        <tr>
+                            <td>${value.Subject}</td>
+                            <td>${value.Message}</td>
+                            <td>${value.DateCreate}</td>
+                            <td>${value.Sender}</td>
+                        </tr>
+                    `);
+                });
+            }
+        });
+    }; 
+    </script>
+</body>
+</html>
